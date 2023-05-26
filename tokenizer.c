dc585cdd2fc6ba8b017802ec4ca9cae05d2277a1
riikakelvin <riikakelvin2@gmail.com>
2023-05-26 10:48:32 -0700

    commit for shell project

--- tokenizer.c
+++ tokenizer.c
@@ -9,42 +9,42 @@
 
 char **strtow(char *str, char *d)
 {
-	int i, j, k, m, numwords = 0;
+	int t, a, k, r, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	if (!d)
 		d = " ";
-	for (i = 0; str[i] != '\0'; i++)
-		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
-			numwords++;
+	for (t = 0; str[t] != '\0'; t++)
+		if (!is_delim(str[t], d) && (is_delim(str[t + 1], d) || !str[t + 1]))
+		 numwords++;
 
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (i = 0, j = 0; j < numwords; j++)
+	for (t = 0, a = 0; a < numwords; a++)
 	{
-		while (is_delim(str[i], d))
-			i++;
+		while (is_delim(str[t], d))
+			t++;
 		k = 0;
-		while (!is_delim(str[i + k], d) && str[i + k])
+		while (!is_delim(str[t + k], d) && str[t + k])
 			k++;
-		s[j] = malloc((k + 1) * sizeof(char));
-		if (!s[j])
+		s[a] = malloc((k + 1) * sizeof(char));
+		if (!s[a])
 		{
-			for (k = 0; k < j; k++)
+			for (k = 0; k < a; k++)
 				free(s[k]);
 			free(s);
 			return (NULL);
 		}
-		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
-		s[j][m] = 0;
+		for (r = 0; r < k; r++)
+			s[a][r] = str[t++];
+		s[a][r] = 0;
 	}
-	s[j] = NULL;
+	s[a] = NULL;
 	return (s);
 }
 
@@ -56,40 +56,40 @@ char **strtow(char *str, char *d)
  */
 char **strtow2(char *str, char d)
 {
-	int i, j, k, m, numwords = 0;
+	int t, a, k, r, numwords = 0;
 	char **s;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		if ((str[i] != d && str[i + 1] == d) ||
-				    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+	for (t = 0; str[t] != '\0'; t++)
+		if ((str[t] != d && str[t + 1] == d) ||
+				    (str[t] != d && !str[t + 1]) || str[t + 1] == d)
 			numwords++;
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (i = 0, j = 0; j < numwords; j++)
+	for (t = 0, a = 0; a < numwords; a++)
 	{
-		while (str[i] == d && str[i] != d)
-			i++;
+		while (str[t] == d && str[t] != d)
+			t++;
 		k = 0;
-		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+		while (str[t + k] != d && str[t + k] && str[t + k] != d)
 			k++;
-		s[j] = malloc((k + 1) * sizeof(char));
-		if (!s[j])
+		s[a] = malloc((k + 1) * sizeof(char));
+		if (!s[a])
 		{
-			for (k = 0; k < j; k++)
+			for (k = 0; k < a; k++)
 				free(s[k]);
 			free(s);
 			return (NULL);
 		}
-		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
-		s[j][m] = 0;
+		for (r = 0; r < k; r++)
+			s[a][r] = str[t++];
+		s[a][r] = 0;
 	}
-	s[j] = NULL;
+	s[a] = NULL;
 	return (s);
 }
 
